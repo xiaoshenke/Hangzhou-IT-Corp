0ca1dbfdccf70b8dd6eb1c1b6722f6aa403995bc
xiaoshenke <1205914137@qq.com>
2017-05-03 17:26:47 +0800

    map

--- ITCorpApp/app/src/main/java/wuxian/me/itcorpapp/MainActivity.java
+++ ITCorpApp/app/src/main/java/wuxian/me/itcorpapp/MainActivity.java
@@ -1,12 +1,28 @@
 package wuxian.me.itcorpapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
 
 import java.util.List;
 
@@ -19,13 +35,133 @@ public class MainActivity extends BaseActionbarActivity {
     @BindView(R.id.map)
     MapView mapView;
 
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+    //public AMapLocationListener mLocationListener = new AMapLocationListener();
+
+    public AMapLocationClientOption mLocationOption = null;
+
+    AMap aMap;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ButterKnife.bind(this);
         mapView.onCreate(savedInstanceState);// 此方法必须重写
-        AMap aMap = mapView.getMap();
+        aMap = mapView.getMap();
+
+        if (checkPermission()) {
+            doLoacMap();
+        }
+    }
+
+    private void doLoacMap() {
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        mLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation amapLocation) {
+                if (amapLocation != null) {
+                    if (amapLocation.getErrorCode() == 0) {
+                        mLocationClient.stopLocation();
+
+                        LatLng latLng = new LatLng(amapLocation.getLatitude(), amapLocation.getLongitude());
+                        Marker marker = aMap.addMarker(new MarkerOptions().position(latLng));  //WORKED !
+
+                    } else {
+                        Log.e("AmapError", "location Error, ErrCode:"
+                                + amapLocation.getErrorCode() + ", errInfo:"
+                                + amapLocation.getErrorInfo());
+                    }
+                }
+            }
+        });
+
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //mLocationOption.setOnceLocation(true);
+        mLocationOption.setInterval(100);
+        mLocationOption.setWifiActiveScan(true);
+        mLocationOption.setNeedAddress(true);
+        mLocationOption.setHttpTimeOut(20000);
+        mLocationOption.setLocationCacheEnable(false);
+
+        mLocationClient.setLocationOption(mLocationOption);
+
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();
+        mLocationClient.startLocation();
+
+        //aMap.setMyLocationStyle(myLocationStyle);
+        //aMap.setMyLocationEnabled(true);      //Fixme: Not Working
+
+    }
+
+    private static final int REQUEST_PERMISSIONS = 101;
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_PERMISSIONS);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doLoacMap();
+                } else {
+
+
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+
+        if (mLocationClient != null) {
+            mLocationClient.onDestroy();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
